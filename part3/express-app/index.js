669217343ad2a5c7309b2ea089137e496deb6b01
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-31 19:27:08 -0300

    3.1

--- part3/express-app/index.js
+++ part3/express-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World! version 2!</h1>");
+  res.send("<h1>Hello World! version 3 pulled directly by Watchtower!</h1>");
 });
 
 const PORT = 8080;
