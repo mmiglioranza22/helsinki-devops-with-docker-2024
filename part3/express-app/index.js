3a08d9ea3bc1e28543044bd39011f77eef8065c7
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-31 19:31:03 -0300

    test watchtower

--- part3/express-app/index.js
+++ part3/express-app/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World! version 3 pulled directly by Watchtower!</h1>");
+  res.send(
+    "<h1>Hello World! version 3 pulled directly by Watchtower, second attempt!</h1>"
+  );
 });
 
 const PORT = 8080;
